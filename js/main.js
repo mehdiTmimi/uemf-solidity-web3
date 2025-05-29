0ec00d0b4461cb01a35bcbc7036ceeb1eab664b8
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-05-29 11:13:49 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const main = async () => {
     const web3 = new Web3(window.ethereum);
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+    }
+    catch (e) {
+        alert("please install metamask alike extensions")
+        console.error(e)
+        return
+    }
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
-    console.log(account)
+    let solde = await web3.eth.getBalance(account)
+    
+    balance.innerText=parseInt(solde)/10**18,"ETH"
+    myAddress.innerText= account
 }
 main()
