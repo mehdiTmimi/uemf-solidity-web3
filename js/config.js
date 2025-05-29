92413db579b446e7e16af34c5e6a7a7ec3d2251d
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-05-29 11:28:53 +0100

    ok

---
+++ js/config.js
@@ -0,0 +1,113 @@
+const contractAddr = "0x213F8fe74Fa5b7011160be6ee31f9078a068F53B"
+const ABI = 
+[
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "_targetAmount",
+					"type": "uint256"
+				},
+				{
+					"internalType": "address",
+					"name": "_recipient",
+					"type": "address"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"stateMutability": "payable",
+			"type": "fallback"
+		},
+		{
+			"inputs": [],
+			"name": "amountLeft",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "donate",
+			"outputs": [],
+			"stateMutability": "payable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "_amount",
+					"type": "uint256"
+				}
+			],
+			"name": "ethToWei",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "pure",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "getTargetAmount",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "isActive",
+			"outputs": [
+				{
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "receipient",
+			"outputs": [
+				{
+					"internalType": "address",
+					"name": "",
+					"type": "address"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "redeem",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"stateMutability": "payable",
+			"type": "receive"
+		}
+	]
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const main = async () => {
     const web3 = new Web3(window.ethereum);
     try {
@@ -11,8 +12,15 @@ const main = async () => {
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
     let solde = await web3.eth.getBalance(account)
-    
+
     balance.innerText=parseInt(solde)/10**18,"ETH"
     myAddress.innerText= account
+    
+    console.log(contractAddr,ABI)
+    const contract = new web3.eth.Contract(ABI, contractAddr);
+
+    const receipient = await contract.methods.receipient().call()
+    console.log("receipient",receipient)
+    console.log(await contract.methods.ethToWei(10).call())
 }
 main()
